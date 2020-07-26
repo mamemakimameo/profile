293ab427a0c34433926b15ad2338d7fce7aec197
karmy <mamemakimameo23@yahoo.co.jp>
2020-07-26 11:35:14 +0900

    [add]新規ファイル追加

---
+++ README.md
@@ -0,0 +1,2 @@
+# profile
+This is Yuki Arima's profile.
---
+++ resources/js/script.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+  /* For the sticky navigation */
+  $('.js--section-promise').waypoint(function(direction){
+    if(direction == "down"){
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  },{
+    offset: '60px;'
+  });
+  /* Scroll on buttons */
+  $('.js--scroll-to-about').click(function(){
+    $('html, body').animate({scrollTop: $('.js--section-promise').offset().top},1000);
+  });
+
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
+  /* Animations on scroll */
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+      offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction) {
+      $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+      offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function(direction) {
+      $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+      offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function(direction) {
+      $('.js--wp-4').addClass('animated fadeIn');
+  }, {
+      offset: '50%'
+  });
+
+    /* Mobile navigation */
+    $(document).on('click',function(e) {
+      var nav = $('.js--main-nav');
+      var nav_icon = $('.reorder-four-outline');
+
+      if(nav_icon.attr('name')=="reorder-four-outline")
+      {
+        if($(e.target).closest('.mobile-nav-icon').length){
+          nav_icon.attr('name','close-outline'); 
+          nav.slideToggle(200);
+        }
+    } else {
+        nav_icon.attr('name','reorder-four-outline'); 
+        nav.slideToggle(200);
+      }
+    });
+});
